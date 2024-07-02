0210622d4d1605b006e327e2c0b9d7046696445c
Ralph Etienne <ralphyetienne@yahoo.com>
2024-07-02 15:14:01 -0400

    Populated database and table with dummy values of student array

--- test.py
+++ test.py
@@ -3,15 +3,28 @@ import mysql.connector
 mydb = mysql.connector.connect(
     host="localhost",
     user="root",
-    passwd="123456",
+    passwd="12345678",
     database="testdb"
 )
 
+
 mycursor = mydb.cursor()
 
-# mycursor.execute("CREATE DATABASE testdb") ### Created database
+
 
 # mycursor.execute("SHOW DATABASES")  ## Show the database created
 
-mycursor.execute("SHOW TABLES")
+# mycursor.execute("SHOW TABLES")   ## Show the table created
+
+
+
+sqlFormula = "INSERT INTO students (name, age) VALUES (%s, %s)"
+students = [("Smith", 22),
+            ("Bill", 31),
+            ("Chance", 22),
+            ("Jessica", 26),
+            ("Nicole", 33)]
+# mycursor.execute(sqlFormula, student1)  ## Execute for line of data
+mycursor.executemany(sqlFormula, students)
+mydb.commit()
 
