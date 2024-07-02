81f83a0fb3bb67cfdaa782ac264733f4512c8b79
Ralph Etienne <ralphyetienne@yahoo.com>
2024-07-02 12:16:44 -0400

    Created Database and students table

--- test.py
+++ test.py
@@ -3,6 +3,17 @@ import mysql.connector
 mydb = mysql.connector.connect(
     host="localhost",
     user="root",
-    passwd="5347978er"
+    passwd="5347978er",
+    database="testdb"
 )
-print(mydb)
+
+mycursor = mydb.cursor()
+
+# mycursor.execute("CREATE DATABASE testdb") ### Created database
+
+# mycursor.execute("SHOW DATABASES")  ## Show the database created
+
+mycursor.execute("SHOW TABLES")
+
+for tb in mycursor:
+    print(tb)
