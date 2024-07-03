f84373d444b1f9e84e656756fbbd82ff345df30f
Ralph Etienne <ralphyetienne@yahoo.com>
2024-07-03 03:20:33 -0400

    Updating Entries and Limiting Queries

--- test.py
+++ test.py
@@ -17,25 +17,39 @@ mycursor = mydb.cursor()
 # mycursor.execute("SHOW TABLES")   ## Show the table created
 
 
-
+# Populate Database
 # sqlFormula = "INSERT INTO students (name, age) VALUES (%s, %s)"
 # students = [("Smith", 22),
 #             ("Bill", 31),
 #             ("Chance", 22),
 #             ("Jessica", 26),
 #             ("Nicole", 33)]
-# mycursor.execute(sqlFormula, student1)        ## Execute for line of data
+# mycursor.execute(sqlFormula, students)        ## Execute for line of data
 # mycursor.executemany(sqlFormula, students)
+
 # mycursor.execute("SELECT * FROM students")       #execute all students from DB through Python
 # sql = "SELECT * FROM students WHERE age=26"     #SELECT a specifc query which holds a specific value
-sql = "SELECT * FROM students WHERE name LIKE 'Je%'"   ## Wildcard
-mycursor.execute(sql)
-myResults = mycursor.fetchall()
+# sql = "SELECT * FROM students WHERE name LIKE 'Je%'"   ## Wildcard
+# mycursor.execute(sql)
+# myResults = mycursor.fetchall()
 
 # mycursor.execute("SELECT age FROM students")     #SELECTs all students age from DB through Python
 # myResults = mycursor.fetchall()
 # myResults = mycursor.fetchone()    ## Fetchs a single row in the database
-for result in myResults:              ## Loop through each row
-    print(result)
+# for result in myResults:              ## Loop through each row
+#     print(result)
 # mydb.commit()          ## commit change in database when using the INSERT query
+
+Update Entries
+sql = "UPDATE students SET age=33 WHERE name='Chance'"
+mycursor.execute(sql)
+
+mydb.commit()
+
+Limit the amount of values
+mycursor.execute("SELECT * FROM students LIMIT 3 OFFSET 2")
+myResult = mycursor.fetchall()
+for i in myResult:
+    print(i)
+
 
