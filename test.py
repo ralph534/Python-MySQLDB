d642285a526c6f6dc22d90026e43f71c7c6b5b12
Ralph Etienne <ralphyetienne@yahoo.com>
2024-07-03 02:34:30 -0400

    Query Conditions with WHERE and Wildcards

--- test.py
+++ test.py
@@ -18,19 +18,24 @@ mycursor = mydb.cursor()
 
 
 
-sqlFormula = "INSERT INTO students (name, age) VALUES (%s, %s)"
-students = [("Smith", 22),
-            ("Bill", 31),
-            ("Chance", 22),
-            ("Jessica", 26),
-            ("Nicole", 33)]
+# sqlFormula = "INSERT INTO students (name, age) VALUES (%s, %s)"
+# students = [("Smith", 22),
+#             ("Bill", 31),
+#             ("Chance", 22),
+#             ("Jessica", 26),
+#             ("Nicole", 33)]
 # mycursor.execute(sqlFormula, student1)        ## Execute for line of data
 # mycursor.executemany(sqlFormula, students)
 # mycursor.execute("SELECT * FROM students")       #execute all students from DB through Python
-mycursor.execute("SELECT age FROM students")     #SELECTs all students age from DB through Python
+# sql = "SELECT * FROM students WHERE age=26"     #SELECT a specifc query which holds a specific value
+sql = "SELECT * FROM students WHERE name LIKE 'Je%'"   ## Wildcard
+mycursor.execute(sql)
+myResults = mycursor.fetchall()
+
+# mycursor.execute("SELECT age FROM students")     #SELECTs all students age from DB through Python
 # myResults = mycursor.fetchall()
-myResults = mycursor.fetchone()    ## Fetchs a single row in the database
-for row in myResults:              ## Loop through each row
-    print(row)
+# myResults = mycursor.fetchone()    ## Fetchs a single row in the database
+for result in myResults:              ## Loop through each row
+    print(result)
 # mydb.commit()          ## commit change in database when using the INSERT query
 
