278af996b1323c1756344dd7d0529b1602af1a66
Ralph Etienne <ralphyetienne@yahoo.com>
2024-07-02 21:23:56 -0400

    Selecting and getting data

--- test.py
+++ test.py
@@ -24,7 +24,13 @@ students = [("Smith", 22),
             ("Chance", 22),
             ("Jessica", 26),
             ("Nicole", 33)]
-# mycursor.execute(sqlFormula, student1)  ## Execute for line of data
-mycursor.executemany(sqlFormula, students)
-mydb.commit()
+# mycursor.execute(sqlFormula, student1)        ## Execute for line of data
+# mycursor.executemany(sqlFormula, students)
+# mycursor.execute("SELECT * FROM students")       #execute all students from DB through Python
+mycursor.execute("SELECT age FROM students")     #SELECTs all students age from DB through Python
+# myResults = mycursor.fetchall()
+myResults = mycursor.fetchone()    ## Fetchs a single row in the database
+for row in myResults:              ## Loop through each row
+    print(row)
+# mydb.commit()          ## commit change in database when using the INSERT query
 
